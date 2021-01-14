6ba91fd76bf649b82a86b645340f2c64dfbb8822
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2021-01-14 13:19:46 +0100

    Add versioneer.

--- freq_tools/__init__.py
+++ freq_tools/__init__.py
@@ -1,4 +1,8 @@
-from ._version import __version__
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
 from . import transfer_functions
 from . import freq_data
 from . import time_data
@@ -6,6 +10,13 @@ from . import freq_models
 from . import io
 from .time_data import CounterData
 from .freq_data import SpectrumAnalyzerData, OscillatorNoise
-from .transfer_functions import  TransferFunction, MachZehnderTransferFunction
-from .freq_models import FreqModel, JohnsonNoise, PhotonShotNoise, NoiseFloor, BetaLine, PowerLawNoise
+from .transfer_functions import TransferFunction, MachZehnderTransferFunction
+from .freq_models import (
+    FreqModel,
+    JohnsonNoise,
+    PhotonShotNoise,
+    NoiseFloor,
+    BetaLine,
+    PowerLawNoise,
+)
 from .io import import_csv, import_json
